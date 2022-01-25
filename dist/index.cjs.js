43aa955c640a70bf6f347e1bdd341d662db2ddf7
Prabhash <re-prabhash@mobiloitte.com>
2022-01-25 15:45:50 +0530

    re build as gettign error while importing icon

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -358,7 +358,7 @@ var Icon$p = function (props) {
 
 var Icon$q = function (props) {
     return (React__default['default'].createElement(Svg, __assign({ viewBox: "0 0 32 32" }, props),
-        React__default['default'].createElement("image", { width: "32", height: "32", href: "/images/egg/logo.png" })));
+        React__default['default'].createElement("image", { width: "32", height: "32", href: "/images/logo.png" })));
 };
 
 var Icon$r = function (props) {
